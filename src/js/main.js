9c22686904e12014af3c64633c274410ffb598c9
claradelvecchio <clara.anquetil@studentfr.ch>
2025-09-18 17:14:29 +0200

    just bonus & change cards

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,14 @@ window.addEventListener("resize", function () {
     menu.classList.remove("is-active");
   }
 });
+
+const toggleDarkModeBtn = document.querySelector("#dark-mode-btn");
+const toggleLightModeBtn = document.querySelector("#light-mode-btn");
+
+toggleDarkModeBtn.addEventListener("click", function () {
+  document.querySelector("html").setAttribute("data-theme", "dark");
+});
+
+toggleLightModeBtn.addEventListener("click", function () {
+  document.querySelector("html").setAttribute("data-theme", "light");
+});
